21c18c63e204443352f5d78afd3aecafd67fcf34
22tarynb <143820704+22tarynb@users.noreply.github.com>
2025-04-16 23:24:54 +0200

    Add Task5-Taryn

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,15 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    //Task 5 Taryn
+
+    public <T extends Comparable<T>> boolean Task5(List<T> inLst){
+        for(int i = 0; i < inLst.size()-1; i++){
+            if(inLst.get(i).compareTo(inLst.get(i+1))>0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
